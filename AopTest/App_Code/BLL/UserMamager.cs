354ecc8a5cda807ff674bcb33419e24bf549df0d
sunkaixuan <610262374@qq.com>
2018-02-07 14:23:53 +0800

    -

--- AopTest/App_Code/BLL/UserMamager.cs
+++ AopTest/App_Code/BLL/UserMamager.cs
@@ -21,6 +21,7 @@ namespace AopTest
         [LogFilter]
         public virtual bool ValidateUser(string name, string pass)
         {
+            Console.WriteLine("执行：当前方法");
             //模拟出异常
             if (name == "nqicecoffee")
                 throw new ArgumentOutOfRangeException("name越界");
