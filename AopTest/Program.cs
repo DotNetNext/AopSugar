6e87d33fe3d0b3355c41212e059171731d337ec7
sunkaixuan <610262374@qq.com>
2018-02-13 18:23:12 +0800

    -

--- AopTest/Program.cs
+++ AopTest/Program.cs
@@ -12,17 +12,17 @@ namespace AopTest
         static void Main(string[] args)
         {
             var factory = AopContainer.Instance;
- 
+
+
+            //类
+            Console.WriteLine("\r\n***********类使用AOP***********");
+            bool flag = factory.CreateInstance<UserMamager>().ValidateUser("张三", "123");
+
+            //接口
             factory.Bind<IUserMamager>().To<UserMamager>();
-            var user = factory.CreateInstance<UserMamager>();
             var iUser = factory.CreateInstance<IUserMamager>();
 
-
-            //下面开始一般拦截示例
-            Console.WriteLine("\r\n***********类AOP***********");
-            bool flag = user.ValidateUser("张三", "123");
-
-            Console.WriteLine("\r\n***********接口AOP***********");
+            Console.WriteLine("\r\n***********接口使用AOP***********");
             bool flag2 = iUser.ValidateUser("管理员", "admin");
 
        
